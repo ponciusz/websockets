6e361b930627869e83f4a1f47068a5194aa4bab8
Claudiu Soare <claudiu.soare@savewatersavemoney.com>
2019-02-25 12:53:07 +0000

    style login form

--- client/src/views/Pages/Login/Login.styles.tsx
+++ client/src/views/Pages/Login/Login.styles.tsx
@@ -2,13 +2,38 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   position: relative;
-  list-style-type: none;
-  box-shadow: 0 0 0 2px gray;
-  height: 200px;
+  height: 100vh;
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-top: 50%;
 `;
 
-export default Wrapper;
+const Container = styled.div`
+  height: 400px;
+  width: 500px;
+  box-shadow: 0 0 0 1px gray;
+  border-radius: 5px;
+`;
+
+const Title = styled.div`
+  background: red;
+  padding: 10px 0;
+  text-align: center;
+  color: white;
+  font-weight: 600;
+  font-size: 24px;
+`;
+
+const Input = styled.input`
+  border-width: 0 0 1px 0;
+  border-color: black;
+  width: 100%;
+  margin: 20px 0;
+`;
+
+const InputLabel = styled.label`
+  color: black;
+`;
+
+export default { Wrapper, Container, Title, Input, InputLabel };
