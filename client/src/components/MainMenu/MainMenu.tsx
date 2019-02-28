39c2bac0e1d9bb15609eb7862a15928dbf7f7923
kalbrycht <kamil.albrycht@savewatersavemoney.com>
2019-02-28 09:59:39 +0000

    Main menu

---
+++ client/src/components/MainMenu/MainMenu.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MainMenu = props => {
+  return <div>MAIN MENU</div>;
+};
+
+export default MainMenu;
--- client/src/history.js
+++ client/src/history.js
@@ -1,3 +1,5 @@
 import { createHashHistory } from 'history';
 
-export default createHashHistory({});
+const history = createHashHistory({});
+
+export default history;
